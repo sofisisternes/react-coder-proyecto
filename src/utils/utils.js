e508d44acd4e9a75f4188080ad989874b64f3b5c
Sofia Sisternes <ssisternes2002@hotmail.com>
2023-12-26 17:13:25 -0300

    arreglando vercel

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,10 @@
 import { useState } from "react"
-import Productos from "../mock/data.json"
+import data from "../data.json";
 
 export const pedirDatos = () => {
-
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(Productos)
-        }, 1000)
-    })
-}
+            resolve(data);
+        }, 1000);
+    });
+};
