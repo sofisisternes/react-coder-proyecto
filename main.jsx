1c011dbfddab7c0e38009b2c73f46f5baf769d1c
Sofia Sisternes <ssisternes2002@hotmail.com>
2024-01-02 19:03:16 -0300

    entrega

--- main.jsx
+++ main.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './src/App.jsx'
-import { createRoot } from 'react-dom/client';
 import './src/index.scss'
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-// Renderizar la aplicación dentro de StrictMode
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
+
